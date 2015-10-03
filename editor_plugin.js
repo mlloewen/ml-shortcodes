9f3777f277db49c92cfcdf259037e6e70bc127ee
mlloewen <mlloewen@gmail.com>
2015-10-03 17:08:29 -0600

    tinymce buttons for cleaning span and p tags

--- editor_plugin.js
+++ editor_plugin.js
@@ -1,14 +1,30 @@
-(function() {
-    tinymce.PluginManager.add('custom_mce_button', function(editor, url) {
+(function () {
+    tinymce.PluginManager.add('custom_mce_button', function (editor, url) {
         editor.addButton('custom_mce_button', {
-            text: 'CleanPSpan',
+            text: 'Clean',
+            type: 'menubutton',
             icon: false,
-            onclick: function() {
-                var text = editor.getContent();
-                editor.setContent('pre' + text + 'post');
-            }
+            menu: [
+                {
+                    text: 'Span',
+                    icon: false,
+                    onclick: function () {
+                        var text = editor.getContent(),
+                        re = new RegExp(/<span>|<span\040[a-z|\d|\042|\072|\055|\075|\073|\040|\043]+>|&nbsp;|<\057span>/ig);
+                        text = text.replace(re, "");
+                        editor.setContent(text);
+                    }
+                },
+                {
+                    text: 'p',
+                    icon: false,
+                    onclick: function () {
+                        var text = editor.getContent(),
+                        re = new RegExp(/<p>|<p\040[a-z|\d|\042|\072|\055|\075|\073|\040|\043]+>|&nbsp;|<\057p>/ig);
+                        text = text.replace(re, "");
+                        editor.setContent(text);
+                    }
+                }
+           ]
         });
     });
-
-
-})();
